612ee5130cdd029a3c2c54a87fda951e82982112
s0ham075 <sohamsawant075@gmail.com>
2025-01-03 08:12:39 +0530

    loss

---
+++ loss.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class Loss():
+    def __init__(self,loss,loss_prime):
+        self.loss = loss
+        self.loss_prime = loss_prime
+
+class MSE(Loss):
+    def __init__(self):
+        def mse(y_true,y_pred):
+            return np.mean(np.power(y_true - y_pred,2))
+        
+        def mse_prime(y_true,y_pred):
+            return (2*(y_pred - y_true)) / np.size(y_true)
